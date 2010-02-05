264b537d014fac8e9002ea4ab46d7f32418fd67d
Ian Stevens <istevens@ian-stevens-powerbook-g4-12.local>
2010-02-05 00:09:59 -0500

    Added validation of form items with JQuery form validation plugin.

--- form.js
+++ form.js
@@ -10,15 +10,19 @@ $(function() {
         width: 450,
         modal: true,
         buttons: {
-            'Create an account': function() {
-                var bValid = true;
-                allFields.removeClass('ui-state-error');
+            'RSVP': function() {
+                var submitOptions = {
+                    url: 'rsvp',
+                    type: 'POST',
+                    dataType: 'json',
+                };
 
-                bValid = bValid && checkLength(name,"name",3,16);
-                bValid = bValid && checkLength(email,"email",6,80);
+                // Manually trigger validation
+                if ($("#rsvpForm").validate().form() == true) {
+                    $('#rsvpForm').ajaxSubmit(submitOptions);
+                    allFields.removeClass('ui-state-error');
+                }
 
-                // From jquery.validate.js (by joern), contributed by Scott Gonzalez: http://projects.scottsplayground.com/email_address_validation/
-                bValid = bValid && checkRegexp(email,/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,"eg. ui@jquery.com");
             },
             Cancel: function() {
                 $(this).dialog('close');
@@ -32,4 +36,19 @@ $(function() {
     $('#rsvp').click(function() {
         $('#rsvp-dialog').dialog('open');
     });
+
+    $('#rsvpForm').validate({
+        rules: {
+            recaptcha_response_field: "required"
+        },
+        errorPlacement: function(error, elem) {
+            if(elem.attr("id") == "recaptcha_response_field") {
+                error.insertAfter($('#recaptcha_label'));
+            } else {
+                error.insertBefore(elem);
+            }
+        },
+        focusCleanup: true
+    });
+
 });
